ed8b5728dcc40d58aee67c7fa201aa2530c1bd58
Jiaqi Huang <sea.chelsea@hotmail.com>
2025-12-23 11:10:57 +0100

    assignment is done

--- build_pipeline.py
+++ build_pipeline.py
@@ -1,4 +1,15 @@
 def build_pipeline(operation_names):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    operations = {
+        "double":lambda x:x*2,
+        "triple":lambda x:x*3,
+        "square":lambda x:x*x
+        }
+    for name in operation_names:
+        if name not in operations:
+            raise KeyError
+    def pipeline(x):
+        value = x
+        for name in operation_names:
+            value = operations[name](value)
+        return value
+    return pipeline
